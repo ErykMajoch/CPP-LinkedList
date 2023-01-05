b8fa33f5d9baf7b765904e32ba6db8ebcb76974a
ErykMajoch <eryk.majoch13@gmail.com>
2023-01-05 19:37:40 +0000

    Implemented the linked list node

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+template <class T>
+struct Node {
+    T Head;
+    Node<T> *Tail;
+
+    T GetValue() {
+        return Head;
+    }
+};
+
 int main() {
     std::cout << "Hello World\n";
     return 1;
